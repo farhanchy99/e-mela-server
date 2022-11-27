1b47473d1b870f02a3398f406675897681bdfb12
farhanchy99 <farhanchowdhury1999@gmail.com>
2022-11-28 02:07:37 +0600

    11th commit

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ async function run(){
             res.send(proList);
         })
 
+//Sell Add Products DATA
         app.get('/myproducts', async(req, res)=>{
             let query ={};
             if(req.query.email){
